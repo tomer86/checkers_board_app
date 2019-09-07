c7713a566ad273f6005042becf372bed5f1c0e58
Tomer Elkayam <tomerel@Tomers-MacBook-Pro.local>
2019-09-08 01:53:19 +0300

    Finished working on app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import CheckersForm from './components/CheckersForm';
+import CheckersBoard from './components/CheckersBoard';
+import {BOARD_SIZE} from './constants.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = this.initState();
+    this.initState.bind(this);      
+    this.onAddSoldier.bind(this);
+    this.onRemoveSoldier.bind(this);
+  }
+  onAddSoldier({row,col}) {
+    this.toggoleSoldier({row, col, hasSoldier: true});
+  }
+  onRemoveSoldier({row,col}) {
+    this.toggoleSoldier({row, col, hasSoldier: false});
+  }
+  toggoleSoldier({row, col, hasSoldier}) {
+    if (row !== null && col !== null && row < BOARD_SIZE && col < BOARD_SIZE) {
+      const { squares } = this.state;
+      const index = (row * BOARD_SIZE) + col;
+      squares[index].hasSoldier = hasSoldier;
+      this.setState({ ...squares });
+    }
+  }
+  initState() {
+    const squares = []
+    for (let i = 0; i < (BOARD_SIZE*BOARD_SIZE); i++) {
+      squares.push({ hasSoldier: false });
+    }
+    return { squares };   
+  }
+  render() {
+    return (
+      <div className="App">
+        <CheckersForm onAddSoldier={this.onAddSoldier.bind(this)}
+                      onRemoveSoldier={this.onRemoveSoldier.bind(this)}/>
+        <CheckersBoard squares={this.state.squares}/>
+      </div>
+    );
+  }
 }
 
 export default App;
